0bb7879a12c488e139ac55344faa53004ec919d5
rickie <rcya996001@gmail.com>
2024-09-25 00:00:32 -0400

    edited

--- YeukLaiRickieAu_MAPD714_301458593_ControlFun/ViewController.swift
+++ YeukLaiRickieAu_MAPD714_301458593_ControlFun/ViewController.swift
@@ -11,7 +11,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var numberField: UITextField!
+    
     @IBOutlet weak var sliderLabel: UILabel!
+    
+    @IBOutlet weak var leftSwitch: UISwitch!
+    @IBOutlet weak var rightSwitch: UISwitch!
+    
+    @IBOutlet weak var doSomethingButton: UIButton!
+    
+    @IBAction func toggleControls(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            leftSwitch.isHidden = false
+            rightSwitch.isHidden = false
+            doSomethingButton.isHidden = true
+        } else {
+            leftSwitch.isHidden = true
+            rightSwitch.isHidden = true
+            doSomethingButton.isHidden = false
+        }
+    }
+    
     
     @IBAction func onSliderChange(_ sender: UISlider) {
         sliderLabel.text = "\(lroundf(sender.value))"
